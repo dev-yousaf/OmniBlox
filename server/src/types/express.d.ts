1230c2ec922b27357193e69ebb81c60dc197a570
Dev-Yousaf <muhammadyousuf575@gmail.com>
2026-06-30 19:35:20 +0500

    fix: Express.Multer defined as namespace, not interface

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,17 @@
-import { File } from 'multer';
-
 declare global {
   namespace Express {
-    interface Multer {
-      File: File;
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
     }
   }
 }
